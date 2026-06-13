8788351183e7c437ee1a5172556e2b5a0c940858
DISKIMG <disk.img@onet.eu>
2026-06-13 21:56:16 +0200

    Add basic fb init

--- src/core/main.c
+++ src/core/main.c
@@ -22,12 +22,47 @@
         UTOPIA_VERSION_MAJOR "." UTOPIA_VERSION_MINOR "." UTOPIA_VERSION_PATCH
 #endif
 
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0
+};
+
 void cpu_main() {
     while (true) continue;
 }
 
+static void hcf() {
+    for (;;) {
+        asm ("hlt");
+    }
+}
+
 // void kmain(multiboot_info_t* mbd) {
 void kmain() {
+    if (LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false) {
+        hcf();
+    }
+
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    volatile uint32_t *fb_ptr = framebuffer->address;
+    for (size_t y = 0; y < framebuffer->height; y++) {
+        for (size_t x = 0; x < framebuffer->width; x++) {
+            uint32_t nX = x * 255 / framebuffer->width;
+            uint32_t nY = y * 255 / framebuffer->height;
+            fb_ptr[y * (framebuffer->pitch / 4) + x] = (nY << 8) | nX;
+        }
+    }
+
     // framebuffer_init(mbd);
     printk("Core", "%s", UTOPIA_VERSION);
     
@@ -49,9 +84,7 @@ void kmain() {
     cpu_main();
     */
     
-    for (;;) {
-        asm ("hlt");
-    }
+    hcf();
 }
 
 extern uint8_t ap_alive_table[256];
