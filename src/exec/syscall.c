d5edd555d8129318c5131cd62880cf736d35b16f
Gorciu <gorciuyt@gmail.com>
2026-07-23 19:12:30 +0200

    refactor(exec/syscall): refactor syscall registering

--- src/exec/syscall.c
+++ src/exec/syscall.c
@@ -12,12 +12,18 @@
 //    then:
 //      rdi, rsi, rdx (unsigned)
 
-typedef struct { 
-    void* iov_base; 
-    size_t iov_len; 
+typedef struct {
+    void* iov_base;
+    size_t iov_len;
 } iovec_t;
 
 #define USER_HEAP_MAX 0x0000800000000000ULL
+#define ENOSYS_ERR    (-38)
+
+typedef uintptr_t (*syscall_fn_t)(registers_t* regs, process_t* process, thread_t* thread);
+
+#define SYSCALL_DEFINE(syscall_name) \
+    static uintptr_t syscall_##syscall_name(registers_t* regs, process_t* process, thread_t* thread) 
 
 static uint64_t page_align_up(uint64_t value) {
     return (value + 0xFFFULL) & ~0xFFFULL;
@@ -42,7 +48,7 @@ static int grow_process_brk(process_t* process, uint64_t new_break) {
     return 0;
 }
 
-uintptr_t write(void* data, uintptr_t len, uintptr_t fd) {
+static uintptr_t write_internal(void* data, uintptr_t len, uintptr_t fd) {
     uint64_t result = -1;
 
     switch (fd) {
@@ -53,145 +59,152 @@ uintptr_t write(void* data, uintptr_t len, uintptr_t fd) {
     default:
         break;
     }
-    
+
     return result;
 }
+
+SYSCALL_DEFINE(read) {
+    (void)process; (void)thread;
+
+    switch (regs->rdi) {
+    case 0: {
+        asm volatile ("sti"); // TODO: temporary fix, syscalls
+                              // should not enable interrupts
+        uintptr_t ret = ps2_read((char*)regs->rsi, regs->rdx);
+        asm volatile ("cli");
+        return ret;
+    }
+    default:
+        return ENOSYS_ERR;
+    }
+}
+
+SYSCALL_DEFINE(write) {
+    (void)process; (void)thread;
+    return write_internal((void*)regs->rsi, regs->rdx, regs->rdi);
+}
+
+SYSCALL_DEFINE(writev) {
+    (void)process; (void)thread;
+
+    int fd = regs->rdi;
+    iovec_t* iov = (iovec_t*)regs->rsi;
+    int iovcnt = regs->rdx;
+
+    size_t total = 0;
+
+    for (int i = 0; i < iovcnt; i++) {
+        size_t ret = write_internal(iov[i].iov_base, iov[i].iov_len, fd);
+        total += ret;
+
+        if ((size_t)ret < iov[i].iov_len) {
+            break;
+        }
+    }
+
+    return total;
+};
+
+SYSCALL_DEFINE(brk) {
+    (void)thread;
+
+    uint64_t requested_break = regs->rdi;
+
+    if (!process) {
+        return ENOSYS_ERR;
+    }
+
+    if (requested_break == 0) {
+        return process->brk_current;
+    }
+
+    if (requested_break < process->brk_start || requested_break >= USER_HEAP_MAX) {
+        return process->brk_current;
+    }
+
+    if (requested_break > process->brk_current && grow_process_brk(process, requested_break) != 0) {
+        return process->brk_current;
+    }
+
+    process->brk_current = requested_break;
+    return process->brk_current;
+}
+
+SYSCALL_DEFINE(arch_prctl) {
+    (void)process; (void)thread;
+
+    uint64_t code = regs->rdi;
+    uint64_t addr = regs->rsi;
+
+    switch (code) {
+    case ARCH_SET_FS:
+        write_msr(IA32_FS_BASE, addr);
+        return 0;
+
+    case ARCH_GET_FS: {
+        if (!addr) return -1;
+        *(uint64_t*)addr = read_msr(IA32_FS_BASE);
+        return 0;
+    }
+
+    case ARCH_SET_GS:
+        write_msr(IA32_GS_BASE, addr);
+        return 0;
+
+    case ARCH_GET_GS: {
+        if (!addr) return -1;
+        *(uint64_t*)addr = read_msr(IA32_GS_BASE);
+        return 0;
+    }
+
+    default:
+        return ENOSYS_ERR;
+    }
+}
+
+SYSCALL_DEFINE(exit) {
+    (void)regs; (void)thread;
+
+    if (process && process->pid == 1) {
+        panic("INIT_EXITED", NULL);
+    }
+
+    thread_exit();
+    return 0; 
+}
+
+SYSCALL_DEFINE(stub_unimplemented) {
+    (void)regs; (void)process; (void)thread;
+    // TODO: implement mprotect and futex syscall
+    return 0;
+}
+
+static const syscall_fn_t syscall_table[] = {
+    [0]   = syscall_read,
+    [1]   = syscall_write,
+    [10]  = syscall_stub_unimplemented, 
+    [12]  = syscall_brk,
+    [20]  = syscall_writev,
+    [60]  = syscall_exit,
+    [158] = syscall_arch_prctl,
+    [202] = syscall_stub_unimplemented, 
+    [231] = syscall_exit,
+};
+
+#define SYSCALL_TABLE_SIZE (sizeof(syscall_table) / sizeof(syscall_table[0]))
 
 void syscall_handler(registers_t* regs) {
     thread_t* current_thread = scheduler_get_current_thread();
     process_t* current_process = current_thread->process;
 
+    uint64_t syscall_num = regs->rax;
+
     dprintk("Debug", "Syscall %d invoked", regs->rax);
 
-    switch (regs->rax) {
-    case 0: // read
-        switch (regs->rdi) {
-        case 0:
-            asm volatile ("sti"); // TODO: temporary fix, syscalls 
-                                  // should not enable interrupts
-            regs->rax_i = ps2_read((char*)regs->rsi, regs->rdx);
-            asm volatile ("cli");
-            break;
-        default:
-            regs->rax_i = -38;
-            break;
-        }
-        break;
-    case 1: // write
-        regs->rax_i = write((void*)regs->rsi, regs->rdx, regs->rdi);
-        break;
-    case 10: case 202: {
-        // TODO: implement mprotect and futex syscall
-        break;
-    }
-    case 20: { // writev
-        int fd = regs->rdi;
-        iovec_t* iov = (iovec_t*)regs->rsi;
-        int iovcnt = regs->rdx;
-    
-        size_t total = 0;
-    
-        for (int i = 0; i < iovcnt; i++) {
-            size_t ret = write(
-                iov[i].iov_base,
-                iov[i].iov_len,
-                fd
-            );
-    
-            total += ret;
-    
-            if ((size_t)ret < iov[i].iov_len) {
-                break;
-            }
-        }
-    
-        regs->rax_i = total;
-        break;
-    }
-    case 12: { // brk
-        uint64_t requested_break = regs->rdi;
-
-        if (!current_process) {
-            regs->rax_i = -38;
-            break;
-        }
-
-        if (requested_break == 0) {
-            regs->rax = current_process->brk_current;
-            break;
-        }
-
-        if (requested_break < current_process->brk_start || requested_break >= USER_HEAP_MAX) {
-            regs->rax = current_process->brk_current;
-            break;
-        }
-
-        if (requested_break > current_process->brk_current && grow_process_brk(current_process, requested_break) != 0) {
-            regs->rax = current_process->brk_current;
-            break;
-        }
-
-        current_process->brk_current = requested_break;
-        regs->rax = current_process->brk_current;
-        break;
-    }
-    case 158: { // arch_prctl
-        uint64_t code = regs->rdi;
-        uint64_t addr = regs->rsi;
-    
-        switch (code) {
-        case ARCH_SET_FS:
-            write_msr(IA32_FS_BASE, addr);
-            regs->rax_i = 0;
-            break;
-    
-        case ARCH_GET_FS: {
-            uint64_t fs = read_msr(IA32_FS_BASE);
-            if (!addr) {
-                regs->rax_i = -1;
-                break;
-            }
-    
-            *(uint64_t*)addr = fs;
-            regs->rax_i = 0;
-            break;
-        }
-    
-        case ARCH_SET_GS:
-            write_msr(IA32_GS_BASE, addr);
-            regs->rax_i = 0;
-            break;
-    
-        case ARCH_GET_GS: {
-            uint64_t gs = read_msr(IA32_GS_BASE);
-            if (!addr) {
-                regs->rax_i = -1;
-                break;
-            }
-    
-            *(uint64_t*)addr = gs;
-            regs->rax_i = 0;
-            break;
-        }
-    
-        default:
-            regs->rax_i = -38;
-            break;
-        }
-    
-        break;
-    }
-    case 60: case 231: { // exit
-        if (current_process && current_process->pid == 1) {
-            panic("INIT_EXITED", NULL);
-        }
-
-        thread_exit();
-        break;
-    }
-    default:
-        regs->rax_i = -38;
-        break;
+    if (syscall_num < SYSCALL_TABLE_SIZE && syscall_table[syscall_num]) {
+        regs->rax_i = syscall_table[syscall_num](regs, current_process, current_thread);
+    } else {
+        regs->rax_i = ENOSYS_ERR;
     }
 
     dprintk("Debug", "RAX return value %d", regs->rax_i);
