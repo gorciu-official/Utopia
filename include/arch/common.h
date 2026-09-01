379545185fab2f5dfa4d7a06ae54883eb8a2a05a
Gorciu <gorciuyt@gmail.com>
2026-09-01 11:45:25 +0200

    feat(arch): add (not working) RISC-V 64 architecture

---
+++ include/arch/common.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <types.h>
+#include <constants.h>
+
+#define CPU_ARCH_MAX_CPUS 256
+
+extern void arch_early_init(void);
+extern void arch_ap_init(void);
+extern void arch_boot_aps(uint8_t* ids, int count);
+
+static inline uint32_t current_processor_id(void) {
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
+    uint32_t eax, ebx, ecx, edx;
+    __asm__ volatile ("cpuid" 
+        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+        : "0" (1));
+    return (ebx >> 24);
+#elif ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+    return 0;
+#endif
+}
+
+
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
+#include <arch/x86_64/registers.h>
+#elif ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+// TODO: put something here
+#endif
+
+static inline uintptr_t read_pf_addr(void) {
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
+    uintptr_t value;
+    __asm__ volatile ("mov %%cr2, %0" : "=r"(value));
+    return value;
+#elif ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+    return 0;
+#endif
+}
