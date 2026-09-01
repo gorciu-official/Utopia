033dbf7293714090327a68dd86c2ec6f1414f4d4
Gorciu <gorciuyt@gmail.com>
2026-09-01 12:10:13 +0200

    feat(arch/riscv64): fix panic.c compilation (and a bunch of other things)

--- src/core/panic.c
+++ src/core/panic.c
@@ -8,6 +8,7 @@ static bool panicked = false;
 
 static const char* cpu_exception_name(uintptr_t int_no) {
 #if ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+    // copied from https://git.evalyngoemer.com/evalynOS/evalynOS/src/branch/main/kernel/src/arch/riscv64/cpu/interrupts.c
     static const char* exceptions[] = {
         "Instruction address misaligned",
         "Instruction access fault",
@@ -75,11 +76,14 @@ static const char* cpu_exception_name(uintptr_t int_no) {
 }
 
 void panic(const char* reason, registers_t* regs) {
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
     asm volatile ("cli");
+#endif
     
     printk_remove_console_suspension();
     printk("Core", "\x1b[91mKernel panic\x1b[0m: %s", reason);
     if (regs != NULL) {
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
         printk("Core", "  - CPU exception: %s", cpu_exception_name(regs->int_no));
         printk("Core", "  - Basic info:    interrupt_number=%d   apic_cpu_id=%d  err_code=%p", regs->int_no, current_processor_id(), regs->err_code);
         printk("Core", "  - Registers:     rax=%p  rbx=%p  rcx=%p  rdx=%p", regs->rax, regs->rbx, regs->rcx, regs->rdx);
@@ -91,7 +95,15 @@ void panic(const char* reason, registers_t* regs) {
                 "Core", "  - Page fault:    %s mode, %s %p", (regs->err_code & (1 << 2)) ? "user" : "kernel",
                 (regs->err_code & (1 << 1)) ? "writing to" : "reading", read_pf_addr()
             );
-        } 
+        }
+#elif ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+        printk("Core", "  - CPU exception: %s", cpu_exception_name(regs->scause));
+        printk("Core", "  - Basic info:    cause=%p  cpu_id=%d  stval=%p", regs->scause, current_processor_id(), regs->stval);
+        printk("Core", "  - Registers:     ra=%p  sp=%p  gp=%p  tp=%p", regs->x[1], regs->x[2], regs->x[3], regs->x[4]);
+        printk("Core", "  - Registers:     a0=%p  a1=%p  a2=%p  a3=%p", regs->x[10], regs->x[11], regs->x[12], regs->x[13]);
+        printk("Core", "  - Registers:     a4=%p  a5=%p  a6=%p  a7=%p", regs->x[14], regs->x[15], regs->x[16], regs->x[17]);
+        printk("Core", "  - Registers:     sepc=%p  stval=%p", regs->sepc, regs->stval);
+#endif
     } else {
         printk("Core", "  - No register dump available, panic not invoked via hardware interrupt.");
     }
@@ -103,10 +115,15 @@ void panic(const char* reason, registers_t* regs) {
     printk("Core", "  - The system enters a halted state, please restart the computer manually.");
 
     while (true)
+#if ARCHITECTURE == ARCHITECTURE_CODE_x86_64
         asm volatile (
             "cli\n"
             "hlt"
         );
+#elif ARCHITECTURE == ARCHITECTURE_CODE_RISCV64
+        // TODO: there should be a disable interrupts thingy
+        asm volatile ("ebreak");
+#endif
 
     // TODO: maybe send init IPIs to APs?
     //
