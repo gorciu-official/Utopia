1640776da5c037826fef1b841925fe3efb0a92b7
Gorciu <gorciuyt@gmail.com>
2026-09-11 19:36:01 +0200

    fix(arch/riscv64): now all userspace shit should get a chance to return from the interrupt or smth idk, there's still a pagefault on startup tho

--- src/arch/riscv64/cpu/interrupts.c
+++ src/arch/riscv64/cpu/interrupts.c
@@ -9,7 +9,14 @@
 extern void arch_interrupt_handler_asm(void);
 extern void timer_schedule_next(registers_t** regs);
 
+static inline void csrw_sscratch(uint64_t v) {
+    __asm__ volatile("csrw sscratch, %0" :: "r"(v));
+}
+
+static uint8_t kernel_stacks[CPU_ARCH_MAX_CPUS][8192];
+
 void arch_init_interrupts() {
+    csrw_sscratch(0);
     csrw(0x105, (uintptr_t)arch_interrupt_handler_asm);
 }
 
@@ -23,6 +30,11 @@ registers_t* arch_interrupt_handler(registers_t* regs) {
         switch (cause) {
         case 5:
             timer_schedule_next(&regs);
+            if (regs->sstatus & (1ULL << 8)) {
+                csrw_sscratch(0);
+            } else {
+                csrw_sscratch((uintptr_t)kernel_stacks[current_processor_id()] + sizeof(kernel_stacks[0]));
+            }
             break;
         default:
             break;
