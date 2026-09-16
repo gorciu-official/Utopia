11d7be4c7ffbde5f3836cd8f10ac50b483e39ee2
Gorciu <gorciuyt@gmail.com>
2026-09-16 10:52:27 +0200

    feat(arch/riscv64): idk bloatware removal

--- src/arch/riscv64/init/limine.c
+++
@@ -1,8 +0,0 @@
-#include <boot/limine.h>
-
-// architecture specific limine settings 
-
-__attribute__((used))
-const volatile struct limine_paging_mode_request paging_mode = {
-    .max_mode = 39, .min_mode = 39
-};
